63a2a30bc95bd9b93556ebf8dbb7312e07d0622d
Robert La Plant <rlaplantjr@sbcglobal.net>
2016-11-20 16:09:20 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?(array, pos)
+  return !position_taken?(array, pos) && pos.between?(0,8)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array, pos)
+  return !(array[pos] == " " || array[pos] == "" || array[pos] == nil)
+end
