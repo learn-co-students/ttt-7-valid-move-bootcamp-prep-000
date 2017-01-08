53c2ead0001ef833cd8754783260373c7403d1df
Grant MacManus <flatiron@macdav.net>
2017-01-08 19:05:00 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
-# code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board,index)
+    true
+  end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == ""
+    true
+  end
+end
