07259c3f3aa1eaec6f15657d5bd5149dbfed72b9
Brian Donovan <bhaus316@aol.com>
2017-03-03 13:39:52 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
-
+def valid_move?(board, input)
+  # index = input - 1 assumed this should be used to convert input to index
+  if input.between?(0, 8) && position_taken?(board, input) == false
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index_number)
+  if (board[index_number] == "X") || (board[index_number] == "O")
+    return true
+  elsif (board[index_number] == " ") || (board[index_number] == "") || (board[index_number] == nil)
+    return false
+  end
+end
