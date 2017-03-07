5cf3f2002ec1301e7599169e8f5fa6ea70f68a2d
Jason  Rosso <jasonrosso@gmail.com>
2017-03-07 03:37:58 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-if !position_taken?(board,index)&&index.between?(0,8)
-true
-else
-  false
-end
+!position_taken?(board,index)&&index.between?(0,8)
+
 end
 
 
