1200fb6a5c50c7ae8d41e62c41bcf801a7e7d28f
Abdul Rahman Quwatli <aznq2005@yahoo.com>
2017-03-23 23:01:52 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if index > 9 && index < 1
+  if index >= 8 && index <= 0
     false
   elsif !(position_taken?(board, index))
     true
