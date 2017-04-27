c571d214460dc61272fc51d063179a0f33b0141f
Alexandra <superbiscuit@gmail.com>
2017-04-27 13:45:59 -0700

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,30 @@
 # code your #valid_move? method here
-
+def valid_move?(board, input)
+	index = input 
+	if index > 8 || index < 0
+		return false
+	end
+position_taken?(board, index) == false
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# code your #position_taken? method here!
+def position_taken?(board, index)
+	if board[index] == " "
+		return false
+	elsif board[index] == ""
+		return false
+	elsif board[index] == nil
+		return false
+	else
+		return true
+	end
+end
+
+def input_to_index(str)
+  int = str.to_i
+    if int == 0
+      return -1
+    end
+  return int - 1
+end
