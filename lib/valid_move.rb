71753d7e43c3a89edb2a9c1d5462c2ee1d47d7d2
Kariny Neves Parreira de Vasconcelos Vasconcelos <karinyparreira@hotmail.com>
2017-07-21 18:03:02 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,27 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if (position_taken?(board, index) == false) && (between?(index) == true)
+    true
+  else
+      false
+  end
+end
 
+def between?(index)
+  if (index > -1 && index < 9)
+    true
+  else
+    false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  else
+    if (board[index] == "X" || board[index] == "O")
+      true
+    end
+  end
+end
--- spec/valid_move_spec.rb
+++ spec/valid_move_spec.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/valid_move.rb"
+require_relative './spec_helper.rb'
 
 describe './lib/valid_move.rb' do
 
