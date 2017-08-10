0bc036b82d3d283f66d0c3d8ee597312484808d2
Yosa Yon <yosa.yon@gmail.com>
2017-08-10 04:35:43 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,8 @@
 def valid_move?(board, index)
-#   if index.between?(1,9)
-#     if !position_taken?(board, index)
-  #     true
-#     end
-#   end
- #end
 
   index.between?(0,8) && !position_taken?(board, index)
 end
+
 def position_taken?(board, index)
   board[index] != " "
 end
