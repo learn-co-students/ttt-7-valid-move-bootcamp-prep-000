e8b381ae62038a1e365474ec37306b385a55ee9a
Joshua James <burke.joshua.james@gmail.com>
2017-09-14 04:30:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  return false if valid?(board, index)
-  true
-end
-
-def valid?(board, index)
-  position_taken?(board, index) || board[index].nil? || index < 0
-end
-
-def position_taken?(board, index)
   move = board[index]
-  return true if move == 'X' || move == 'O'
-  false
+  (invalid?(move) || index < 0) ? false : true
+end
+
+def invalid?(move)
+  position_taken?(move) || move.nil?
+end
+
+def position_taken?(move)
+  (move == 'X' || move == 'O') ? true : false
 end
