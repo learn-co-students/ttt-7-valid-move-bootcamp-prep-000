7a5af0f752b543eb9422e35600fdeba92c0523b8
Kenny Hom <kenny.hom27@gmail.com>
2017-09-19 22:56:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
+def position_taken(board, index)
+  board[index] == " " || board[index] == "" || board[index] == nil ? false : true
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken(board, index)
+end
