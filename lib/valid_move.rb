cf5fee21328d92557724202d4d9eb87c5eb15a0b
Anthony Grimes <agrimes@charter.net>
2017-09-28 17:31:18 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if board[index] == "X" || board[index] == "O" || index > 8
+    if board[index] == "X" || board[index] == "O" || index > 8 #could use index.between(0, 8)
         false
     else
         true
