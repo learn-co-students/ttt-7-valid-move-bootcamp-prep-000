f904cabbb0e45389d9571995d586b45b6c09f236
Tiago Severino <tiago.mseverino@gmx.com>
2017-10-10 14:13:20 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(1, 9)
+  if index.between?(0, 9)
     if !position_taken?(board, index)
       true
     else
