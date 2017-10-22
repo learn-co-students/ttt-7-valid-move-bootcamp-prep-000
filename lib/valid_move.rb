0f9e8196ab88881b7907c37ba7e7ee3aeb9550ac
Jacob Miller <jake0miller@gmail.com>
2017-10-22 06:47:45 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
-def valid_move?
-
+def valid_move?(board,index)
+  if index.between?(0,8) && !position_taken(board,index)
+    return true
+  end
+  return false
 end
 
 def position_taken(board,index)
