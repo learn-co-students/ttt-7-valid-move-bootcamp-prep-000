d3ea4393cf24919f9c48144b5c1b678b3be5c4e3
Yuanleoluo <leoyuanluo@gmail.com>
2017-11-03 21:46:57 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
-
+def valid_move?(a, b)
+  if b>9 || b<0
+    false
+  else
+    if position_taken?(a, b)
+      false
+    else
+      true
+    end
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(a, b)
+  if a[b] === " "
+    false
+  elsif a[b] === ""
+    false
+  elsif a[b] === nil
+    false
+  elsif a[b] === "O" || a[b] === "X"
+    true
+  end
+end
