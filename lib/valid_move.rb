578c1116212ac3a0210bd5a76b214ea475c49bb0
lsiler23 <siler.laurette@gmail.com>
2018-01-04 19:16:54 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  (0..8).include?(index) && !position_taken?(board, index)
+end 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  marks = ["X", "O"]
+  marks.any? {|mark| board[index] == mark }
+end 
