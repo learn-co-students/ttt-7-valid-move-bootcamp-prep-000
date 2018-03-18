c1210edc66b65d4f5061a12c2a82f72e61274895
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 03:27:33 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,29 @@
 # code your #valid_move? method here
-
+def valid_move?(board, idx)
+  good_idx = idx.between?(0, 8)
+  good_position = !position_taken?(board, idx)
+  
+  good_idx && good_position 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, idx)
+  len = board.length
+  if len == 0
+    return false
+  end
+  
+  out = board[idx]
+  
+  if out == nil
+    return false  
+  end
+  
+  out_strip = out.strip
+  if out_strip == "X" || out_strip == "O"
+    return true
+  else
+    return false
+  end
+end
