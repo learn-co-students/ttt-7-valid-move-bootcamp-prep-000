739dc15f263d4056c8f0862a2773abad75ffd132
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-24 09:20:53 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,6 @@ def valid_move?(arr, ind)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(arr, ind)
   (arr[ind] == "" || arr[ind] == " " || arr[ind] == nil) ?
     false : true
