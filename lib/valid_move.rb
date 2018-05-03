ed690d655a91e72512b9547c9f4dd147eca54bf5
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-03 00:55:53 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,8 +4,7 @@ def valid_move?(board,index)
         return false
     elsif index.between?(0,8) && !position_taken?(board,index)
         return true
-    else
-        return nil
+  
     end
 end
 
