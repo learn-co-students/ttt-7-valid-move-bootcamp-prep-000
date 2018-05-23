4bb0640a74b285c5eb79640d3e85e2f126c5075d
Nathan Hill <nateohill@gmail.com>
2018-05-23 16:22:27 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
-
+def valid_move?(arr, index)
+  if position_taken?(arr, index) === false
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(arr, index)
+  if arr.length < index 
+    return true
+  elsif arr[index] === "" || arr[index] === " " || arr[index] === nil
+    return false
+  else
+    return true
+  end
+end
