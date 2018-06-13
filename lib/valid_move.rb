987db1e824733f5c21bf1b0ddeff8d2aae305c83
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 18:28:00 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index > board.length 
     false
   else
-    position_taken?(board, index)
+    !position_taken?(board, index)
   end
 end
 
@@ -11,10 +11,10 @@ end
 
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " "
-    true
-  elsif board[index].nil?
-    true
-  elsif board[index] == "O" || board[index] == "X"
     false
+  elsif board[index].nil?
+    false
+  elsif board[index] == "O" || board[index] == "X"
+    true
   end
 end
