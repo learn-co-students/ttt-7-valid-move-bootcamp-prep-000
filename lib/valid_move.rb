446c1a692cf442474c555c15503e570e49783d1c
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-14 13:51:05 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index <= 8
+  true if position_taken?(board, index) == false && index <= 8
     true
   elsif position_taken?(board, index) == true && index <= 8
     false
