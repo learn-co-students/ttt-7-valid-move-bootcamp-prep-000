0e28093076eb2ad3bc5187ba94ce1530618edebb
Lung Chen <lungheichen@gmail.com>
2018-07-30 02:20:31 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,8 @@
 # code your #valid_move? method here
-
+def valid_move?(num)
+  if num.between(0, 8)
+    return true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
