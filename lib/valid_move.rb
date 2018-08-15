9938e355dd1350a1b24c9144b956066827bcddcd
valentine-m@live.fr <valentine-m@live.fr>
2018-08-15 16:52:34 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,5 +12,5 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  board[index] != " "
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
