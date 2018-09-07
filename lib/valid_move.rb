50ad31468e49370af1279cbf031d447d6fe5d66d
Sam B <s77broz@aol.com>
2018-09-07 00:05:54 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  !position_taken?(board, index) && index < 9
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  ["X", "O"].include?(board[index])
+end
