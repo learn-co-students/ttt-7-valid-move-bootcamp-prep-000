a275ff48fcc1330944a89a851cb12aceea5a0c27
viqi zevic <vicky.tanzil@gmail.com>
2018-09-11 08:33:09 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,15 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  cell = board[index]
+  cell != " " && cell != "" && cell != nil
+end
+
+def valid_move?(board, index)
+  if index < 0 || 9 <= index
+    return false
+  end
+  not position_taken?(board, index)
+end
