546de191fede37d4f282766cdf50fdaecd94ac64
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-01 12:37:38 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,16 @@
 def valid_move?(array, index)
-
-if array[index] == " "
-  return true
+  if index.between?(0,8) && position_taken?(array, index) == false
+    return true
+    
+  else
+    return false
+  end
   
 end
-end
 
 
-def position_taken?(array,index)
-  if array[index] == " " || array[index] == "" || array[index] == nil
-    return false
-  elsif array[index] == "X" || array[index] == "O"
-    return true
+def position_taken?(array, index)
+  if array[index] == "" || array[index] == " " || array[index] == nil
+  return false
   end
 end
