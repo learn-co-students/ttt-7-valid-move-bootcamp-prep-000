8a01797d8613a228e3a51b46ab406309c05bafd4
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-09 15:14:39 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,6 @@
 # code your #valid_move? method here
-
-
+valid_move?(pos)
+  return false if pos < 0 || pos > 9
+    true
+end 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
