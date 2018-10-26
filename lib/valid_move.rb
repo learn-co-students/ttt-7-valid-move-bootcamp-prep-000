87108c30ef8efcce52ef345d2fc590ca0689f2b6
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-26 00:42:34 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-
-  if position_taken?(board, index) && (index > 8 || index < 0)
+  if index > 8 || index < 0
+    return false
+  end
+  if position_taken?(board, index)
     return false
   else return true
   end
