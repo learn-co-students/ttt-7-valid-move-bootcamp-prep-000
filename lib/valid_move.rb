a0de67a74240c411eb33bb83ee21c7c050fa578a
stubrew24 <sewelly@gmail.com>
2019-01-14 10:52:30 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,10 @@
+require 'pry'
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if (0..8).include?(index) && board[index] == " "
+    true
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
