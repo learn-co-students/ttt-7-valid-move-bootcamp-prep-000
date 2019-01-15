ea1f433596d6081a6a242d1b975fb83f6af43f19
github username <github email address>
2019-01-15 17:20:23 -0500

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,34 @@
 # code your #valid_move? method here
 
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+
+  # range = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  # if board.include?("X" || "O") && position_taken?(board,index) == false
+  #   true
+  # elsif !board.include?("X" || "O") && position_taken?(board,index) == false
+  #   true
+  # elsif range.include?(index) == false
+  #   false
+  # elsif
+  # end
+end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
