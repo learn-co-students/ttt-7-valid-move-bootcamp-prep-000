bbafceaa43a565dfaa963f8a6553be5361f36ea6
Ivan Szebenszki <szib77@gmail.com>
2019-03-04 14:11:19 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?(board, idx)
+  return true if (0..8).include?(idx) && !position_taken?(board, idx)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, idx)
+  return true unless (board[idx] == " " || board[idx] == "" || board[idx] == nil)
+end
