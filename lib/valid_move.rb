8236ee9ed3bebf875f1c77f113733b1d32937d3c
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-10 21:35:35 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  if(position_taken(board, index) == false && index < 10)
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken(board, index)
+  if(board[index] == "" || board[index] == nil || board[index] == " ")
+    return false
+  else
+    return true
+  end
+end
+
