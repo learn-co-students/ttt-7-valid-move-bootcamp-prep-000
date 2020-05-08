49d74e6d2407beaa19219750b0c1d1a7fe012d0b
Molly Cooper <mollycooper64@gmail.com>
2020-05-08 20:16:49 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,29 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
 
-#if index is 0-8 = true
-#if position? = true then return true
+#returns true for a valid position on an empty board
+#board[index] == be_between?(0,8) && !position_taken?(board, index)
 
-board[index].between(0,8) = true
-
-
-
+index.between?(0,8) && !(position_taken?(board, index))
 
 end
+
+
+
+
+#returns true for a valid position on a non-empty board
+
+
+
+
+#returns nil or false for an occupied position
+
+
+#returns nil or false for a position that is not on the board
+
+
+
+
 
 
 
